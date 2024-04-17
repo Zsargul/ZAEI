ef3d4efd678c1b23451601a9faaf36e9915e238a
Zsargul <kubakerlino@gmail.com>
2024-04-17 20:27:37 +0100

    added constants.h

---
+++ src/constants.h
@@ -0,0 +1,10 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define MAX_STR_LEN 200
+
+const char *GIT_DOTS_URL = "";
+const char *PACKAGES_LIST = "packages_list.csv";
+const char *AUR_HELPER = "yay";
+
+#endif
