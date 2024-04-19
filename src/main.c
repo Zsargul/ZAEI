df6de19f9161e879fce064c42f1ff60c54a3dfda
Zsargul <kubakerlino@gmail.com>
2024-04-19 16:39:43 +0100

    added more command lines options

--- src/main.c
+++ src/main.c
@@ -1,25 +1,38 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <getopt.h>
 
 int main(int argc, char **argv) {
 	int opt;
 	
 	/* Argument flags */
-	int csvFlag = 0;
+	int c_flag = 0;
+	int d_flag = 0;
+	int h_flag = 0;
+	int debug_flag = 0;
+
+	char* c_value = NULL;
+	char* d_value = NULL;
+
+	/* Possible short arguments */
+	const char *optstring = "c:d:";
 
 	struct option longOptions[] = {
-		{"csv", no_argument, &csvFlag, 1},
+		{"csv", 	required_argument, 	&c_flag, 	'c'},
+		{"dwm", 	required_argument, 	$d_flag, 	'd'},
+		{"help", 	no_argument, 		&h_flag, 	'h'},
+		{"debug",	no_argument,		$dbg_flag,	0},
 		{0, 0, 0, 0}
 	};
 
-	while ((opt = getopt_long(argc, argv, "c", long_options, NULL)) != -1) {
+	while ((opt = getopt_long(argc, argv, "c", longOptions, NULL)) != -1) {
 		switch (opt) {
 			case 'c': /* -c */
+				c_flag = 1;
 				break;
-			case 0: /* --csv */
-				if (csvFlag) {
-					/* Handle --csv flag */
-				}
+			case 'd': /* -d */
+				d_flag = 1;
+				d_value = optarg;
 				break;
 			case '?': /* Invalid option or missing argument */
 				help();
@@ -27,8 +40,14 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	if (!csvFlag) {
-		fprintf(stderr, "path to .csv package list is required\n");
+	/* Check for required arguments */
+	if (!c_flag) {
+		fprintf(stderr, "Path to .csv package list is required\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (!d_flag) {
+		fprintf(stderr, "URL to dwm repo is required\n");
 		exit(EXIT_FAILURE);
 	}
 
