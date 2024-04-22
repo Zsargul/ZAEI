c2187c11c4b0024ad373688a9984e1ac359697d5
Zsargul <kubakerlino@gmail.com>
2024-04-22 16:45:43 +0100

    more todos and minor changes

--- src/packages.c
+++ src/packages.c
@@ -9,26 +9,33 @@
 #include "packages.h"
 #include "parsecsv.h"
 
+/* TODO: Stick to one naming convention for variables for christ's sake */
 int install_packages() {
-	int no_of_packages = csv_lines(PACKAGES_LIST_FILE);
-	if (no_of_packages == -1)
+	int packageCount = csv_lines(PACKAGES_LIST_FILE);
+	if (packageCount == -1)
 		exit(EXIT_FAILURE);
 
-	Package pkgs[no_of_packages]; /* Array of packages to install */
+	Package pkgs[packageCount]; /* Array of packages to install */
 
-	if (no_of_packages != (sizeof(pkgs) / sizeof(pkgs[0]))) {
+	if (packageCount != (sizeof(pkgs) / sizeof(pkgs[0]))) {
 		fprintf(stderr, "Error creating pkgs array. Exiting.\n");
 		exit(EXIT_FAILURE);
 	}
 
-	if (parse_package_list(PACKAGES_LIST_FILE, pkgs, no_of_packages) == -1)
+	if (parse_package_list(PACKAGES_LIST_FILE, pkgs, packageCount) == -1)
 		exit(EXIT_FAILURE);
 
-	for (size_t i = 0; i < no_of_packages; i++) {
+	int successfulInstalls = 0;
+	int officialPkgsCount = 0;
+	int aurPkgsCount = 0;
+
+	/* TODO: Create tidier print messages using return characters etc. */
+	for (size_t i = 0; i < packageCount; i++) {
 		Package* pkgPtr = &pkgs[i];
 
 		int ret = install_package(pkgPtr);
 		
+		fprintf(stdout, "[%d/%d]", i+1, packageCount);
 		if (ret != 0 && pkgPtr->req) {
 			fprintf(stderr, "%zd: Package %s, was not installed successfully, but it is not required.\n", i, *(pkgs[i].name));
 		} else if (ret != 0 && !(pkgPtr->req)) {
@@ -36,8 +43,21 @@ int install_packages() {
 			exit(EXIT_FAILURE);
 		} else {
 			fprintf(stdout, "%zd: %s installed successfully\n", i, *(pkgs[i].name));
+
+			successfulInstalls++;
+			(pkgPtr->onAur) ? aurPksCount++ : officialPkgsCount++;
 		}
 	}
+
+	fprintf(stdout,
+			"Successfully installed %d/%d packages.\n
+			Packages from official repositories: %d\n
+			Packages from Arch user repository: %d\n",
+			successfulInstalls,
+			packageCount,
+			officialPksCount,
+			AurPkgsCount);
+
 
 
 	return 0;
@@ -54,11 +74,11 @@ int install_package(Package *pkg) {
 
 	const pid_t forkPid = fork();
 
+	/* TODO: Rethink return values for this? */
 	switch (forkPid) {
 		case -1:
 			const int forkErrno = errno; /* Save error code */
-			fprintf(stderr, "Could not fork new process: %s\n",
-					strerror(forkErrno));
+			fprintf(stderr, "Could not fork new process: %s\n", strerror(forkErrno));
 			return forkErrno;
 		case 0: /* Child process */
 			fprintf(stdout, "Child process executing: %s...\n", cmd);
@@ -90,6 +110,5 @@ int install_package(Package *pkg) {
 			fprintf(stdout, "Child process with PID %d exited with a status of %d\n", forkPid, childResult);
 			break;
 	}
-
 	return 0;
 }
