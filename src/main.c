421ca4e1cb9df79638693e3d9e0ef57a11896a6b
Zsargul <kubakerlino@gmail.com>
2024-04-26 14:55:23 +0100

    added todo and config file

--- src/main.c
+++ src/main.c
@@ -7,6 +7,8 @@
 
 #include "util/miscutils.h"
 
+/* TODO: Include libonfig system path in makefile instead of just copying the header into include/. Same
+ * goes for .a library */
 int main(int argc, char **argv) {
 
 	return EXIT_SUCCESS;
