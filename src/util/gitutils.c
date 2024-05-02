d05502bd37d1943a6881460d2fa49431023e5a81
Zsargul <kubakerlino@gmail.com>
2024-05-02 20:39:54 +0100

    made clone_repo silent

--- src/util/gitutils.c
+++ src/util/gitutils.c
@@ -30,11 +30,12 @@ int git_installed() {
  */
 int clone_repo(const char* name, const char* repoUrl, const char* targetDir) {
 	/* TODO: Output the possible error "targetDir already exists and is not empty" */
+	int ret = 1;
 
 	/* Check if git is installed */
 	if (git_installed() != 0) {
 		fprintf(stderr, "Git is not installed/configured correctly on your system. Please install git before running this tool with any git repositories specified in the config.\n");
-		return 1;
+		ret = 1;
 	}
 
 	/* Create specified directory if it does not exist */
@@ -43,12 +44,11 @@ int clone_repo(const char* name, const char* repoUrl, const char* targetDir) {
 		fprintf(stdout, "%s: Directory %s does not exist. Creating it...\n", name, targetDir);
 		if (_mkdir(targetDir) != 0) {
 			fprintf(stderr, "%s: Error creating directory %s.\n", name, targetDir);
-			return 1;
+			ret = 1;
 		}
 	}
 
 	/* Download repo */
-	char command[MAX_STR_LEN];
 	const char* cmdArgs[] = {
 		[0] = "git",
 		[1] = "clone",
@@ -63,10 +63,10 @@ int clone_repo(const char* name, const char* repoUrl, const char* targetDir) {
 		case -1:
 			const int forkErrno = errno;
 			fprintf(stderr, "Could not fork new process: %s\n", strerror(forkErrno));
-			ret = -1;
+			ret = 1;
 		case 0:
 			/* Pipe stdout and stderr to /dev/null */
-			int devnull = open("/dev/null". O_WRONLY);
+			int devnull = open("/dev/null", O_WRONLY);
 
 			dup2(devnull, 1);
 			close(1);
@@ -78,27 +78,31 @@ int clone_repo(const char* name, const char* repoUrl, const char* targetDir) {
 
 			assert(execResult == -1);
 			int const execErrno = errno;
-			fprintf(stderr, "repo_exists(): Error from execvp() - %s\n", strerror(execErrno));
+			fprintf(stderr, "clone_repo(): Error from execvp() - %s\n", strerror(execErrno));
 
-			ret = -1;
+			ret = 1;
 		default:
 			int status;
-			int returnedPid = waitPid(forkPid, &status, 0);
-	
+			int returnedPid = waitpid(forkPid, &status, 0);
+			if (returnedPid) { /* Child exited successfully */
+				assert(forkPid == returnedPid);
+
+				dbg_fprintf(stdout, "clone_repo(): Child exited with code %d\n", status);
+				if (WIFEXITED(status) && !WEXITSTATUS(status)) { /* Repo was cloned successfully */
+					dbg_fprintf(stdout, "repo_exists(): Git command failure\n", status);
+					ret = 0;
+				} else if (WIFEXITED(status) && WEXITSTATUS(status)) {
+					dbg_fprintf(stdout, "clone_repo(): Git command success\n", status);
+					ret = -1; /* Repo doesn't exist */
+				}
+			} else {
+				int waitpidErrno = errno;
+				fprintf(stderr, "Error waiting for PID %d: %s\n", waitpidErrno, strerror(waitpidErrno));
+				ret = -1;
+			}
 	}
-	/* TODO: Change this to a fork()/exec() call and redirect output to /dev/null */
-	//int ret = system(command);
-
-	//if (ret == 0) {
-	//	fprintf(stdout, "%s: Repository successfully cloned.\n", name);
-	//} else {
-	//	fprintf(stderr, "%s: Unable to clone repository.\n", name);
-	//	return 1;
-	//}
-
-	return 0;
+	return ret;
 }
-
 /* Return values:
  * -1: Error
  *  0: Repo exists
