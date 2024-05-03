129b67cad045cadfe21810e9d50f87274e530cf7
Zsargul <kubakerlino@gmail.com>
2024-05-03 13:41:38 +0100

    moved some stuff around between utils

--- include/constants.h
+++ include/constants.h
@@ -2,13 +2,14 @@
 #define CONSTANTS_H
 
 #define MAX_STR_LEN 1024
+#define LOG_MSG_LEN 7
 #define AUR_HELPER "yay"
 #define CSV_FIELDS 3
 #define CONFIG_FILE "zaei.conf"
 
 /* Error strings */
 #define WARN "WARNING"
-#define CRIT "CRITICAL ERROR"
+#define ERR "ERROR"
 #define INFO "INFO"
 
 #endif
--- include/util/miscutils.h
+++ include/util/miscutils.h
@@ -2,11 +2,7 @@
 #define MISCUTILS_H
 
 int _mkdir(const char* dir);
-void enable_debug_mode();
-int dbg_fprintf(FILE *stream, const char *format, ...);
 int dir_not_empty(const char* dirname);
-void err_usage(char* progName, char* errStr);
-void usage(char* progName);
 char* libconfig_version();
 
 #endif
